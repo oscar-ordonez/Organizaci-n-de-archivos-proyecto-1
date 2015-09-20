1812bf80380600f00132785612b48692ec55cfb2
nelcar <nelsonjc.01@gmail.com>
2015-09-20 17:44:26 -0600

    Segmentation fault

--- main2.cpp
+++ main2.cpp
@@ -24,12 +24,15 @@ using namespace std;
 
 */
 int posicionIndice(vector <string>, unsigned long);
+int indiceOrdenado(vector<string> llaveIndice  , int llave);
 
+const int HeaderSize = sizeof(int) + sizeof(int) + sizeof(bool);
 void convertirPersonas();
 void indexarPersonas();
 void mostrarPersonas();
 
 void convertirRegistros();
+void agregarPersona();
 
 /*
 
@@ -57,6 +60,9 @@ int main(int argc, char* argv[]){
 			cin >> 	op;
 			if(op == 1){
 				 mostrarPersonas();
+			}
+			if(op==2){
+				agregarPersona();
 			}		
 			//convertirRegistros();
 		}
@@ -117,6 +123,56 @@ int posicionIndice(vector <string> llaveIndice, unsigned long llave){
     	}			
 	}
 	return -1;
+}
+int indiceOrdenado(vector<string> llaveIndice  , int llave){
+    int bajo = 0;
+    int alto = llaveIndice.size() - 1;
+    int medio;
+    bool bandera = false;
+    
+    while(true){
+    	if(bajo > alto)
+    		break;    	
+    	medio = (bajo + alto)/2;
+    	if(llave == atoi((llaveIndice.at(medio)).c_str()) )
+    		return medio;
+    	if(llave > atoi((llaveIndice.at(medio)).c_str())){
+    		if(medio != llaveIndice.size() - 1){
+    			if(llave < atoi((llaveIndice.at(medio+1)).c_str())){
+    				return medio+1;
+    			}else{
+    				if(bandera && medio == llaveIndice.size()-2)
+    					return -1;
+    				bajo = medio;
+    				bandera = true;
+    			}
+    		}else{
+    			if(llave < atoi((llaveIndice.at(medio)).c_str())){
+    				return medio;
+    			}else{
+    				return -1;
+    			}
+
+    		}
+    	}else{
+    		if(medio != 0){
+    			if(llave > atoi((llaveIndice.at(medio-1)).c_str())){
+
+    				return medio;
+    			}else{
+    				alto = medio;
+    			}
+    		}else{
+    			if(llave < atoi((llaveIndice.at(medio)).c_str())){
+    				return medio;
+    			}else{
+    				return medio+1;
+    			}
+
+    		}
+    	}
+    }
+    return -1;
 }
 void convertirPersonas(){
 	//archivo texto
@@ -265,6 +321,124 @@ void mostrarPersonas(){
 	readFile.close();
 }
 
+void agregarPersona(){
+	ifstream archivo("persona.bin", ios::binary);
+	char id[15];
+	char nombre[40];
+	char genero[2];
+	char ciudad[6];
+	archivo.seekg(0);
+	int header, recorrido,cont = 0;
+	archivo.read(reinterpret_cast<char*>( header), sizeof(int));
+	archivo.read(reinterpret_cast<char*>(&recorrido), sizeof(int));
+	archivo.seekg(HeaderSize + header*( sizeof(id) + sizeof(nombre) + sizeof(genero) + sizeof(ciudad))+ sizeof(id) );
+	archivo.read(reinterpret_cast<char*>(&nombre), sizeof(nombre));
+	stringstream streamNombre;
+	for (int i = 0; i < sizeof(nombre); ++i){
+		streamNombre << nombre[i];
+	}
+	int nuevo = atoi(streamNombre.str().c_str());
+	cout << nuevo << "°|°" << endl;
+	archivo.close();
+	string leerName = "",leerId = "", leergenero = "",leerCiudad = "";
+	if (header != -1){
+		cout << "Ingrese el Id del cliente: ";
+		cin >> leerId;
+		stringstream ss;
+		for (int i = 0; i < sizeof(id); ++i){
+			id[i] = leerId[i];
+			ss<< leerId[i];
+		}
+		
+		cout << "Ingrese el nombre del cliente: ";
+		cin >> leerName;
+		for (int i = 0; i < sizeof(nombre); ++i){
+			nombre[i] = leerName[i];
+		}
+
+		cout << "Ingrese el genero del cliente: ";
+		cin >> leergenero;
+		for (int i = 0; i < sizeof(genero); ++i){
+			genero[i] = leergenero[i];
+
+		}
+
+		cout << "Ingrese la ciudad en la que el cliente vive: ";
+		cin >> leerCiudad;
+		for (int i = 0; i < sizeof(ciudad); ++i){
+			ciudad[i] = leerCiudad[i];
+		}
+		cout << "paso ciudad" << endl;
+		ofstream writeFile("personas.bin", ofstream::in | ofstream :: out);
+		cout << "paso 1" << endl;
+		writeFile.seekp(HeaderSize + header*( sizeof(id) + sizeof(nombre) + sizeof(genero) + sizeof(ciudad))  );
+		cout << "paso 2" << endl;
+		writeFile.write(reinterpret_cast<char*>(&id), sizeof(id));
+		cout << "paso 3" << endl;
+		writeFile.write(reinterpret_cast<char*>(&nombre), sizeof(nombre));
+		cout << "paso 4" << endl;
+		writeFile.write(reinterpret_cast<char*>(&genero), sizeof(genero));
+		cout << "paso 5" << endl;
+		writeFile.write(reinterpret_cast<char*>(&ciudad), sizeof(ciudad));
+		cout << "paso 6" << endl;
+		writeFile.seekp(0);
+		cout << "paso 7" << endl;
+		writeFile.write(reinterpret_cast<char*>(&nuevo), sizeof(nuevo));
+		cout << "paso 8" << endl;
+		writeFile.close();
+		cout << "paso 9" << endl;
+		int position = indiceOrdenado( llaveCliente  , atoi(ss.str().c_str()) );
+		int num = llaveCliente.begin()+ position;
+		cout << "position:" << position << "begin" <<"paso 10" << endl;
+		llaveCliente.insert(llaveCliente.begin()+ position, ss.str());
+		cout << "paso 11" << endl;
+	}else{
+		cout << "Ingrese el Id del cliente: ";
+		cin >> leerId;
+		stringstream ss;
+		for (int i = 0; i < sizeof(id); ++i){
+			id[i] = leerId[i];
+			ss<< leerId[i];
+		}
+		
+		cout << "Ingrese el nombre del cliente: ";
+		cin >> leerName;
+		for (int i = 0; i < sizeof(nombre); ++i){
+			nombre[i] = leerName[i];
+		}
+
+		cout << "Ingrese el genero del cliente: ";
+		cin >> leergenero;
+		for (int i = 0; i < sizeof(genero); ++i){
+			genero[i] = leergenero[i];
+		}
+
+		cout << "Ingrese la ciudad en la que el cliente vive: ";
+		cin >> leerCiudad;
+		cout << "paso ciudad" << endl;
+		for (int i = 0; i < sizeof(ciudad); ++i){
+			ciudad[i] = leerCiudad[i];
+		}
+		cout << "paso for ciudad" << endl;
+		cout << "paso" << endl;
+		ofstream writeFile("personas.bin", ofstream::in | ofstream::out);
+		cout << "paso1" << endl;
+		writeFile.seekp(HeaderSize + header*( sizeof(id) + sizeof(nombre) + sizeof(genero) + sizeof(ciudad)) );
+		cout << "paso2" << endl;
+		writeFile.write(reinterpret_cast<char*>(&id), sizeof(id));
+		cout << "paso3" << endl;
+		writeFile.write(reinterpret_cast<char*>(&nombre), sizeof(nombre));
+		cout << "paso4" << endl;
+		writeFile.write(reinterpret_cast<char*>(&genero), sizeof(genero));
+		cout << "paso5" << endl;
+		writeFile.write(reinterpret_cast<char*>(&ciudad), sizeof(ciudad));
+		cout << "paso6" << endl;
+		writeFile.close();
+	}
+}
+
+
+
 void convertirRegistros () {
 	ifstream archivo("registros.txt");
 	ofstream archivoBinario("registros.bin");
@@ -334,4 +508,5 @@ void convertirRegistros () {
 		}
 		leerArchivo.close();
 	}
+
 }
